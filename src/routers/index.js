aad2aaef56c68ccd89468d3511247ce9ed15b6cd
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-20 22:57:57 +0300

    fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import authRouter from './auth.js';
 import ByIdrecipesRouter from './getRecipeById.routes.js'; // працюе
+import sevedRouter from './addToSaved';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/recipes', sevedRouter);
 router.use('/recipes', ByIdrecipesRouter); // працюе
 
 export default router;
