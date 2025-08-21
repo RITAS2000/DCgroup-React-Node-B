b58db27555620c36802f8cbc2695d2a1a8038501
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-08-22 02:38:06 +0300

    change middlewares

--- src/routers/delSavedRecipe.js
+++ src/routers/delSavedRecipe.js
@@ -1,14 +1,14 @@
 import Router from 'express';
 
-import { protect } from '../middlewares/auth.middleware.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { delSavedRecipeController } from '../controllers/delSavedRecipeController.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
 router.delete(
   '/saved-recipes/:recipeId',
-  protect,
+  authenticate,
   ctrlWrapper(delSavedRecipeController),
 );
 
