390097983a9b1a8459302dee2cd7b657599bae74
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-21 07:10:29 +0300

    fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,20 +3,18 @@ import { Router } from 'express';
 import authRouter from './auth.js';
 
 import usersRouter from './users.js';
-import categoriesRouter from './categories.js'
-import ingredientsRouter from './ingredients'
+import categoriesRouter from './categories.js';
+import ingredientsRouter from './ingredients';
 import ByIdrecipesRouter from './getRecipeById.routes.js'; // працюе
 import sevedRouter from './addToSaved.js';
-
 
 const router = Router();
 
 router.use('/auth', authRouter);
 
-router.use('/users', usersRouter)
-router.use('/categories', categoriesRouter)
-router.use('/ingredients', ingredientsRouter)
-
+router.use('/users', usersRouter);
+router.use('/categories', categoriesRouter);
+router.use('/ingredients', ingredientsRouter);
 
 router.use('/recipes', ByIdrecipesRouter); // працюе
 router.use('/recipes', sevedRouter);
