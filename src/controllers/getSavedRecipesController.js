8ea93aae456d9d72147a425f43a83dd6e5bbe4bb
Chekan <ky.chekan@gmail.com>
2025-08-22 14:17:51 +0300

    Swagger: fix openapi and saved recipes controller

--- src/controllers/getSavedRecipesController.js
+++ src/controllers/getSavedRecipesController.js
@@ -4,12 +4,36 @@ import { UsersCollection } from '../db/models/user.js';
 export const getSavedRecipesController = async (req, res) => {
   const userId = req.user.id;
 
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 12;
+
   const user = await UsersCollection.findById(userId).populate('savedRecipes');
   if (!user) throw createHttpError(404, 'User not found');
+
+  const totalItems = user.savedRecipes.length;
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+
+  const paginatedRecipes = user.savedRecipes.slice(startIndex, endIndex);
 
   res.json({
     status: 200,
     message: 'User saved recipes fetched successfully',
-    data: user.savedRecipes,
+    data: {
+      data: paginatedRecipes,
+      page,
+      perPage,
+      totalItems,
+      totalPages,
+      hasPreviousPage: page > 1,
+      hasNextPage: page < totalPages,
+    },
   });
+  // res.json({
+  //   status: 200,
+  //   message: 'User saved recipes fetched successfully',
+  //   data: user.savedRecipes,
+  // });
 };
