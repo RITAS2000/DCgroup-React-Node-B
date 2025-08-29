9184cfc2045e538e4441176eee6ac5ce8040782c
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-29 18:11:44 +0300

    fix

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 import { getCurrentUserService } from '../services/users.js';
 
 export async function getCurrentUserController(req, res) {
-  const current = await getCurrentUserService(req.user.id);
+  const current = await getCurrentUserService(req.owner.id);
   if (!current) {
     throw createHttpError(404, 'User not found');
   }
