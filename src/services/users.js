1f64201de5391ce239cdc040ec7d450a7f889aa8
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-29 18:22:43 +0300

    fix

--- src/services/users.js
+++ src/services/users.js
@@ -4,9 +4,10 @@ export async function getCurrentUserService(userId) {
   const user = await UsersCollection.findById(userId);
   if (!user) return null;
 
-  const name = user.name ?? '';
-  const initial = name ? name[0].toUpperCase() : '';
-  return { name, initial };
+  // const name = user.name ?? '';
+  // const initial = name ? name[0].toUpperCase() : '';
+  // return { name, initial };
+  return user;
 }
 
 // ** Тут логіка сервисной функції та знов чітко вказуємо те що нам буде треба для фронта, для залогіненого юзера
